8ce5b27712b51d2489b0f9b341e89748ad46244c
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2024-07-31 07:35:28 -0700

    Maybe?

--- extras/gjb_itn_compat_gplt/CfgITNCompat.hpp
+++ extras/gjb_itn_compat_gplt/CfgITNCompat.hpp
@@ -368,7 +368,7 @@ class CfgWeapons
 			{
 				gjb_itn_memoryPoints[]=
 				{
-					{-0.283867,0.046036,0.106207}
+					{0.029436,-1.76599e-008,0.0951298}
 				};
 			};
 			class Louetta_tran_fsp_base
